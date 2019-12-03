59035e49dbf4f5f25ad250d8aef38ce718065c18
sofieneJ <42573195+sofieneJ@users.noreply.github.com>
2019-12-03 17:10:19 +0000

    removed .bat files

--- start_local_ElMO_server.bat
+++
@@ -1,3 +0,0 @@
-set FLASK_APP=server_ElMO.py
-set FLASK_ENV=development
-call C:\\Users\\sofiene.jenzri\\AppData\\Local\\conda\\conda\\envs\\myAIFabricEnv\\python.exe -m flask run
--- start_server.bat
+++
@@ -1,3 +0,0 @@
-set FLASK_APP=server.py
-set FLASK_ENV=development
-call %1\python.exe -m flask run --host=0.0.0.0
