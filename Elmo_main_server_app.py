02973d94ab64a731fab474e9736c3c495905c730
sofieneJ <42573195+sofieneJ@users.noreply.github.com>
2019-12-07 17:05:36 +0100

    retraining preprocessing

--- Elmo_main_server_app.py
+++ Elmo_main_server_app.py
@@ -168,6 +168,98 @@ class NERExtractor():
         print (json.dumps(predicted_entities, sort_keys=True, indent=2))
         return json.dumps(predicted_entities, sort_keys=True, indent=2)
 
+    def retrain (self, text, validation_results):
+        taxonomy_map_path = './models/taxonomy_mapping.json'
+        with open(taxonomy_map_path, 'r') as taxo_file:
+            taxo_map = json.load(taxo_file)
+
+        json_extraction = json.loads(validation_results)
+        tags_list =[]
+        for entity_group in json_extraction["ResultsDocument"]["Fields"]:
+            for entity in entity_group["values"]:
+                entity_text = entity["Value"]
+                start_index = entity["Reference"]["TextStartIndex"]
+                entity_text_length =  entity["Reference"]["TextLength"]
+                tag = taxo_map [entity_group["FieldId"]]
+                token_start_index = start_index
+                counter = 0
+                for i, token in enumerate(entity_text.split()):
+                    token_start_index = start_index + entity_text.index(token, counter)
+                    token_tag = "B"+tag if i==0 else "I"+tag 
+                    counter += len(token)
+                    my_tag= {
+                        "token":token,
+                        "start_index":token_start_index,
+                        "tag":token_tag
+                    }
+                    tags_list.append(my_tag)
+        
+                    
+def test_retrain ():
+    text="""Indian police are being declared heroes Friday after they shot and killed four men suspected of raping and killing a young woman in southern India just a week earlier.
+
+Last Thursday, the remains of a 27-year-old veterinarian were discovered by a passerby at an underpass in the town of Shadnager, near Hyderabad, after she left her scooter at a toll booth for a medical visit the night before.
+
+The four men -- between the ages of 20 and 26 -- allegedly deflated her scooter and took her to a truck yard to repair it. Soon after, they assaulted and suffocated the woman before burning her body, according to Sky News."""
+    validation_results = """{"DocumentId":"india_news_v.pdf","ResultsVersion":1,"ResultsDocument":{"Bounds":{"StartPage":0,"PageCount":1,"TextStartIndex":0,"TextLength":618},"Language":"","DocumentGroup":"NewsArticle","DocumentCategory":"Politics","DocumentTypeId":"NewsArticle.Politics.article","DocumentTypeName":"article","DocumentTypeDataVersion":0,"DataVersion":1,"DocumentTypeSource":"Automatic","DocumentTypeField":{"Components":[],"Value":"article","Reference":{"TextStartIndex":0,"TextLength":0,"Tokens":[]},"DerivedFields":[],"Confidence":1.0,"OperatorConfirmed":true,"OcrConfidence":1.0},"Fields":[{"FieldId":"NewsArticle.Politics.article.Person","FieldName":"Person","FieldType":"Text","IsMissing":true,"DataSource":"Automatic","Values":[],"DataVersion":0},{"FieldId":"NewsArticle.Politics.article.TimeOrDate","FieldName":"TimeOrDate","FieldType":"Text","IsMissing":false,"DataSource":"ManuallyChanged","Values":[{"Components":[],"Value":"Friday","Reference":{"TextStartIndex":40,"TextLength":6,"Tokens":[{"TextStartIndex":40,"TextLength":6,"Page":0,"PageWidth":612.0,"PageHeight":792.0,"Boxes":[[74.18,250.5975,27.313,11.04]]}]},"DerivedFields":[],"Confidence":1.0,"OperatorConfirmed":true,"OcrConfidence":1.0},{"Components":[],"Value":"Thursday,","Reference":{"TextStartIndex":174,"TextLength":9,"Tokens":[{"TextStartIndex":174,"TextLength":9,"Page":0,"PageWidth":612.0,"PageHeight":792.0,"Boxes":[[133.7,92.4149,43.895,11.04]]}]},"DerivedFields":[],"Confidence":1.0,"OperatorConfirmed":true,"OcrConfidence":1.0},{"Components":[],"Value":"night","Reference":{"TextStartIndex":381,"TextLength":5,"Tokens":[{"TextStartIndex":381,"TextLength":5,"Page":0,"PageWidth":612.0,"PageHeight":792.0,"Boxes":[[162.62,89.5114,22.93,11.04]]}]},"DerivedFields":[],"Confidence":1.0,"OperatorConfirmed":true,"OcrConfidence":1.0}],"DataVersion":1},{"FieldId":"NewsArticle.Politics.article.Organization","FieldName":"Organization","FieldType":"Text","IsMissing":false,"DataSource":"ManuallyChanged","Values":[{"Components":[],"Value":"Sky News.","Reference":{"TextStartIndex":609,"TextLength":9,"Tokens":[{"TextStartIndex":609,"TextLength":9,"Page":0,"PageWidth":612.0,"PageHeight":792.0,"Boxes":[[236.57,130.0613,14.9812,11.04],[236.57,147.5707,27.5007,11.04]]}]},"DerivedFields":[],"Confidence":1.0,"OperatorConfirmed":true,"OcrConfidence":1.0}],"DataVersion":1},{"FieldId":"NewsArticle.Politics.article.GeographicEntity","FieldName":"GeographicEntity","FieldType":"Text","IsMissing":false,"DataSource":"ManuallyChanged","Values":[{"Components":[],"Value":"southern India","Reference":{"TextStartIndex":132,"TextLength":14,"Tokens":[{"TextStartIndex":132,"TextLength":14,"Page":0,"PageWidth":612.0,"PageHeight":792.0,"Boxes":[[88.7,204.3274,40.6161,11.04],[88.7,247.3944,22.1352,11.04]]}]},"DerivedFields":[],"Confidence":1.0,"OperatorConfirmed":true,"OcrConfidence":1.0},{"Components":[],"Value":"Shadnager,","Reference":{"TextStartIndex":287,"TextLength":10,"Tokens":[{"TextStartIndex":287,"TextLength":10,"Page":0,"PageWidth":612.0,"PageHeight":792.0,"Boxes":[[148.1,137.6126,50.205,11.04]]}]},"DerivedFields":[],"Confidence":1.0,"OperatorConfirmed":true,"OcrConfidence":1.0},{"Components":[],"Value":"Hyderabad,","Reference":{"TextStartIndex":303,"TextLength":10,"Tokens":[{"TextStartIndex":303,"TextLength":10,"Page":0,"PageWidth":612.0,"PageHeight":792.0,"Boxes":[[148.1,212.9998,51.866,11.04]]}]},"DerivedFields":[],"Confidence":1.0,"OperatorConfirmed":true,"OcrConfidence":1.0}],"DataVersion":1},{"FieldId":"NewsArticle.Politics.article.GeopoliticalEntity","FieldName":"GeopoliticalEntity","FieldType":"Text","IsMissing":false,"DataSource":"ManuallyChanged","Values":[{"Components":[],"Value":"Indian","Reference":{"TextStartIndex":0,"TextLength":6,"Tokens":[{"TextStartIndex":0,"TextLength":6,"Page":0,"PageWidth":612.0,"PageHeight":792.0,"Boxes":[[74.18,72.024,27.9091,11.04]]}]},"DerivedFields":[],"Confidence":1.0,"OperatorConfirmed":true,"OcrConfidence":1.0}],"DataVersion":1}]}}"""
+
+    taxonomy_map_path = './models/taxonomy_mapping.json'
+    with open(taxonomy_map_path, 'r') as taxo_file:
+        taxo_map = json.load(taxo_file)
+
+    json_extraction = json.loads(validation_results)
+    tags_list =[]
+    for entity_group in json_extraction["ResultsDocument"]["Fields"]:
+        for entity in entity_group["Values"]:
+            entity_text = entity["Value"]
+            start_index = entity["Reference"]["TextStartIndex"]
+            entity_text_length =  entity["Reference"]["TextLength"]
+            tag = taxo_map [entity_group["FieldId"]]
+            token_start_index = start_index
+            counter = 0
+            for i, token in enumerate(entity_text.split()):
+                token_start_index = start_index + entity_text.index(token, counter)
+                token_tag = "B"+tag if i==0 else "I"+tag 
+                counter += len(token)
+                my_tag= {
+                    "token":token,
+                    "start_index":token_start_index,
+                    "tag":token_tag
+                }
+                tags_list.append(my_tag)
+    
+    max_seq_len = 128
+    batch_size = 32    
+    
+    sents = sent_tokenize(text)
+    sents = [sent for sent in sents if len(sent.split())<max_seq_len]
+    nb_sentences = len(sents)
+
+    retraining_text_seq = './new_training_data/hot_folder/text_sequences.txt'
+    retraining_tags_seq = './new_training_data/hot_folder/tags_sequences.txt'
+
+    nb_hits = 0
+    for i,sent in enumerate(sents):
+        tag_sequence= []
+        for j,token in enumerate(sent.split()):
+            index_in_text = text.index(sent, sum([len(s) for k,s in enumerate(sents) if k<i])) + sent.index(token, sum([(len(t)+1) for k,t in enumerate(sent.split()) if k<j ]))
+            hits = list(filter(lambda tag: tag["start_index"]==index_in_text, tags_list))
+            if len(hits) != 0:
+                assert(len(hits)==1)
+                assert (hits[0]["token"]==token)
+                tag_sequence.append(hits[0]["tag"])
+                nb_hits+=1
+            else:
+                tag_sequence.append("O")
+        with open(retraining_text_seq,"a") as f:
+            f.write(sent)
+            f.write("\n")
+        with open(retraining_tags_seq,"a") as f:
+            f.write(','.join(tag_sequence))
+            f.write("\n")
+    assert (len(tags_list)==nb_hits)
+
+
 
 def test_main():
 
@@ -201,21 +293,20 @@ And there's another problem facing many traditional Labour voters here: the part
 
 
 if __name__=='__main__':
-    sample = test_main()
-    sample = """HARTLEPOOL, England - It used to be simple in Hartlepool.
+    # sample = test_main()
+    test_retrain()
 
-Come election time, a majority of voters in this coastal working class town in the northeast of England would inevitably back the left-leaning Labour Party.
-Proud of its industrial history as a center for shipbuilding - the H.M.S. Trincomalee, Europe's oldest floating warship, sits in the town's windswept marina - this is still regarded a Labour heartland as the United Kingdom prepares for an election Dec.
-12.
 
-But old ties are being tested to their limits with a possibly devastating impact on Labour, which is fighting to defeat the ruling Conservative Party.
-Voters in Hartlepool feel they have missed out on the economic growth of the last few decades, which has transformed London and the southeast of England.
-This is the eastern edge of what pollsters have called the "red wall" of Brexit-supporting seats Labour holds with a majority of less than 8,000 votes.
-
-And there's another problem facing many traditional Labour voters here: the party's leader, Jeremy Corbyn.
-
-"I don't think he's a good leader," said Christine Scott, 57, as she prepared some herring for sale in Hodgson's fishmongers, part of a family-owned business that's been in Hartlepool for more than a century. "I'm not 100 percent sure about him.\""""
-
-    print ((sample[310:310+6]))
+    # taxonomy_dic = {
+    #     "NewsArticle.Politics.article.GeopoliticalEntity":"-gpe",
+    #     "NewsArticle.Politics.article.GeographicEntity":"-geo",
+    #     "NewsArticle.Politics.article.Organization" :"-org",
+    #     "NewsArticle.Politics.article.TimeOrDate" :"-tim",
+    #     "NewsArticle.Politics.article.Person":"-per"
+    # }
+    
+    # taxonomy_map_path = './models/taxonomy_mapping.json'
+    # with open(taxonomy_map_path, 'w') as taxo_file:
+    #     taxo_file.write(json.dumps(taxonomy_dic, sort_keys=True, indent=2))
     
 
